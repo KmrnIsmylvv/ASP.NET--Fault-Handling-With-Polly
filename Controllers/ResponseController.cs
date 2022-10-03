9e944cdaac2c6c3f87adbd145fe2c5f8bcb4f4b7
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-10-03 16:12:37 +0400

    feat: Complete Response Service

--- Controllers/ResponseController.cs
+++ Controllers/ResponseController.cs
@@ -8,6 +8,14 @@ namespace ResponseService.Controllers
         [HttpGet]
         public IActionResult GetAResponse(int id)
         {
+            Random rnd = new Random();
+            int rndInteger = rnd.Next(1, 101);
+            if (rndInteger >= id)
+            {
+                Console.WriteLine("--> Failure - Generate a HTTP 500");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            Console.WriteLine("--> Success - Generate a HTTP 200");
             return Ok();
         }
     }
